de11a4aa3318e5e92d7b928773199bd8423d8310
Doug <Github@lanmanager.net>
2023-02-19 23:04:17 -0500

    SCAN DONE
    I now check the callback reason and if it was a scan done interrupt then I exit. Otherwise it will continue on and try to read addr data but a scan done interrupt does not have addr

--- govee.py
+++ govee.py
@@ -2,21 +2,28 @@ import bluetooth
 import time
 import binascii
 
+data = None
+
 ble = bluetooth.BLE()
 
 if ble.active() == False:
     ble.active(True)
 
 def scan_callback(event, addr):
-    global counter
+    if event == 6:
+        print('SCAN DONE')
+        return
+    global data
+    print(event, addr)
     mac_add_string = addr[1]
     mac_add_hex = binascii.hexlify(mac_add_string).decode('utf-8')
+    #print(mac_add_hex)
+    #if mac_add_hex == 'a4c13859da18' or mac_add_hex == 'a4c1388c3622':
     if mac_add_hex == 'a4c13859da18':
-            data = addr[4]
-            
-            #turns binary into string of hex octets
-            data = binascii.hexlify(data).decode('utf-8') 
-            print(data)
+        #turns binary into string of hex octets
+        #data = binascii.hexlify(data).decode('utf-8')
+        print(mac_add_hex, addr)
+
     
 ble.irq(scan_callback)
 ble.gap_scan(5000, 5000000, 5000000)
