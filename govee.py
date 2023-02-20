e2b9b7d08b6a7710c28af1e2c8f45082c4903c84
Doug <Github@lanmanager.net>
2023-02-19 23:21:57 -0500

    Extracting Data
    I have extracted the data that I can now convert into temperature.

--- govee.py
+++ govee.py
@@ -14,15 +14,13 @@ def scan_callback(event, addr):
         print('SCAN DONE')
         return
     global data
-    print(event, addr)
     mac_add_string = addr[1]
     mac_add_hex = binascii.hexlify(mac_add_string).decode('utf-8')
     #print(mac_add_hex)
-    #if mac_add_hex == 'a4c13859da18' or mac_add_hex == 'a4c1388c3622':
-    if mac_add_hex == 'a4c13859da18':
+    if mac_add_hex == 'a4c13859da18' or mac_add_hex == 'a4c1388c3622':
         #turns binary into string of hex octets
-        #data = binascii.hexlify(data).decode('utf-8')
-        print(mac_add_hex, addr)
+        data = binascii.hexlify(addr[4]).decode('utf-8')
+        print(mac_add_hex, data[54:60])
 
     
 ble.irq(scan_callback)
