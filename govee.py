234296be00ad209eb95639d9ad7aecf64374b502
Doug <Github@lanmanager.net>
2023-02-19 23:49:51 -0500

    Celcius is working!
    Celcius is spot on. Next is convert to Farenheit.

--- govee.py
+++ govee.py
@@ -16,11 +16,14 @@ def scan_callback(event, addr):
     global data
     mac_add_string = addr[1]
     mac_add_hex = binascii.hexlify(mac_add_string).decode('utf-8')
-    #print(mac_add_hex)
     if mac_add_hex == 'a4c13859da18' or mac_add_hex == 'a4c1388c3622':
         #turns binary into string of hex octets
         data = binascii.hexlify(addr[4]).decode('utf-8')
-        print(mac_add_hex, data[54:60])
+        #print(mac_add_hex, data[54:60])
+        hex_str = data[52:58]
+        decimal_val = int(hex_str, 16)
+        celcius = decimal_val/10000
+        print(mac_add_hex, hex_str, data, celcius)
 
     
 ble.irq(scan_callback)
