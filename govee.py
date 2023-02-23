95ff034f1728ae62cd691f9c412c80d3d538d8ee
Doug <Github@lanmanager.net>
2023-02-22 19:31:15 -0500

    Several Updates - working
    Made a function to calculate the temperature.
    Made a dictionary where I can store my devices.

--- govee.py
+++ govee.py
@@ -2,35 +2,42 @@ import bluetooth
 import time
 import binascii
 
-data = None
-
 ble = bluetooth.BLE()
+
+my_devices = {'a4c1388c3622': 'Office-1: ',
+              'a4c13859da18': 'Office-2: ' }
+
 
 if ble.active() == False:
     ble.active(True)
-
+           
 def scan_callback(event, addr):
     if event == 6:
         print('SCAN DONE')
         return
-    global data
+    
     mac_add_string = addr[1]
     mac_add_hex = binascii.hexlify(mac_add_string).decode('utf-8')
-    if mac_add_hex == 'a4c13859da18' or mac_add_hex == 'a4c1388c3622':
-        #turns binary into string of hex octets
+    if mac_add_hex in my_devices:
+        temp = get_temp(addr)
+        print(my_devices[mac_add_hex], round(temp, 2))
+
+
+def get_temp(addr):
         data = binascii.hexlify(addr[4]).decode('utf-8')
-        #print(mac_add_hex, data[54:60])
         hex_str = data[52:58]
         decimal_val = int(hex_str, 16)
         celcius = decimal_val/10000
         fahrenheit = celcius * 1.8 + 32
-        print(mac_add_hex, fahrenheit)
+        return fahrenheit
 
-    
+
 ble.irq(scan_callback)
-ble.gap_scan(5000, 5000000, 5000000)
+ble.gap_scan(10000, 1000000, 1000000)
 
-time.sleep(6)
+time.sleep(11)
 
 ble.gap_scan(None)
 ble.irq(None)
+
+
