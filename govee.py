2ab52b878fd7cb4d82b6f1f6b70cbdde81aef6f9
Doug <Github@lanmanager.net>
2023-03-03 15:25:08 -0500

    MANY CHANGES
    Changed my_devices from a dictionary to a list of tuples.
    Am storing the temp in each tuple. I update it on each scan_callback event.

--- govee.py
+++ govee.py
@@ -1,30 +1,16 @@
 import bluetooth
 import time
 import binascii
-#import lvgl as lv
 
 loop_prog = True
-'''
-def create_screen(hex_color):
-  screen = lv.obj()
-  screen.set_style_local_bg_color(lv.obj.PART.MAIN, lv.STATE.DEFAULT, lv.color_hex(hex_color))
-  return screen
 
-scr_home = create_screen(0xdddddd)
-lv.scr_load(scr_home)
-'''
-my_devices = {'a4c1388c3622': 'Desk:       ',
-              'a4c13859da18': 'Thermostat: ',
-              'a4c138133c6c': 'Patio:      ',
-              'a4c1381e4420': 'AC Vent:    ',
-              'a4c138d14b3a': 'Outside:    ',
-              'a4c138c3e807': 'Bathroom    ' }
 
-desk = 0
-thermostat = 0
-patio = 0
-ac_vent = 0
-Outside = 0
+my_devices = [('a4c1388c3622', 'Desk:       ', 0),
+              ('a4c13859da18', 'Thermostat: ', 0),
+              ('a4c138133c6c', 'Patio:      ', 0),
+              ('a4c1381e4420', 'AC Vent:    ', 0),
+              ('a4c138d14b3a', 'Outside:    ', 0),
+              ('a4c138c3e807', 'Bathroom:   ', 0)]
 
 
 
@@ -35,16 +21,26 @@ if ble.active() == False:
 
 
 def scan_callback(event, addr):
+    global my_devices  # Declare my_devices as global
+    
     if event == 6:  #because event 6 has no addr returned
         print('SCAN DONE')
         return
     
     mac_add_string = addr[1]
     mac_add_hex = binascii.hexlify(mac_add_string).decode('utf-8')
-    if mac_add_hex in my_devices:
+    if mac_add_hex in [device[0] for device in my_devices]:
         temp = get_temp(addr)
-        print(my_devices[mac_add_hex], round(temp, 2))
-        return
+        updated_devices = []
+        for device in my_devices:
+            if device[0] == mac_add_hex:
+                updated_device = (device[0], device[1], round(temp, 2))
+                #print(updated_device[1], updated_device[2])
+            else:
+                updated_device = device
+            updated_devices.append(updated_device)
+        my_devices = updated_devices
+
 
 def get_temp(addr):
         data = binascii.hexlify(addr[4]).decode('utf-8')
@@ -62,10 +58,12 @@ ble.gap_scan(0,500000, 500000)
 try:
     while loop_prog:
         time.sleep(6)
+        for device in my_devices:
+            if device[2] == 0:
+                continue
+            print(device[1], device[2])
 except KeyboardInterrupt:
     print('User stopped the program.')
     ble.irq(None)
     ble.gap_scan(None)
-
-
 
