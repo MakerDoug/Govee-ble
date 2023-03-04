7f78d5567681019f10449a89de2fa624b811d4dc
Doug <Github@lanmanager.net>
2023-03-04 17:19:52 -0500

    Cleand up scan_callback

--- govee.py
+++ govee.py
@@ -11,9 +11,11 @@ my_devices = [('a4c1388c3622', 'Desk:       ', 0),
               ('a4c138d14b3a', 'Outside:    ', 0),
               ('a4c138c3e807', 'Bathroom:   ', 0)]
 
+
 ble = bluetooth.BLE()
 if ble.active() == False:
     ble.active(True)
+
 
 def scan_callback(event, addr):
     global my_devices  # Declare my_devices as global
@@ -24,28 +26,22 @@ def scan_callback(event, addr):
 
     mac_add_hex = binascii.hexlify(addr[1]).decode('utf-8')
 
-    if mac_add_hex in [device[0] for device in my_devices]:
-        temp = get_temp(addr)
-        updated_devices = []
-        for device in my_devices:
-            if device[0] == mac_add_hex:
-                updated_device = (device[0], device[1], round(temp, 2))
-            else:
-                #updated_device must have a value, since it has no new data
-                #we will just make it what it currently is.
-                updated_device = device
-            updated_devices.append(updated_device)
-        my_devices = updated_devices
+    for device in my_devices:
+      if mac_add_hex == device[0]:
+         temp = get_temp(addr)
+         device_index = my_devices.index(device)
+         my_devices[device_index] = (device[0], device[1], temp)
+
 
 
 def get_temp(addr):
-        data = binascii.hexlify(addr[4]).decode('utf-8')
-        hex_str = data[52:58]
-        decimal_val = int(hex_str, 16)
-        celcius = decimal_val/10000
-        fahrenheit = celcius * 1.8 + 32
-        fahrenheit_rounded = round(fahrenheit, 1)
-        return fahrenheit_rounded
+    data = binascii.hexlify(addr[4]).decode('utf-8')
+    hex_str = data[52:58]
+    decimal_val = int(hex_str, 16)
+    celcius = decimal_val / 10000
+    fahrenheit = celcius * 1.8 + 32
+    fahrenheit_rounded = int(fahrenheit * 10 + 0.5) / 10
+    return fahrenheit_rounded
 
 ble.irq(scan_callback)
 ble.gap_scan(0,500000, 500000)
@@ -53,16 +49,16 @@ ble.gap_scan(0,500000, 500000)
 try:
     while loop_prog:
         time.sleep(6)
-        
+     
         for device in my_devices:
             if device[2] == 0:
                 continue
             print(device[1], device[2])
         print('\n')
 
+ 
 except KeyboardInterrupt:
     print('User stopped the program.')
     ble.irq(None)
     ble.gap_scan(None)
-
-
+    
